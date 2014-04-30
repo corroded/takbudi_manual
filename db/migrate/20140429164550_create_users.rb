ba8e80e498f72f9b498488f463a3f1e07a400cf1
Eumir Gaspar <eumir@aelogica.com>
2014-04-30 23:04:17 +0800

    Installed devise + home page + users model

--- db/migrate/20140429164550_create_users.rb
+++ db/migrate/20140429164550_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :first_name, :middle_name, :last_name
+      t.string :address1, :address2, :city, :state, :zip
+      t.string :email
+      t.string :contact_number
+      t.string :gender, :shirt_size
+
+      t.datetime :date_of_birth
 
       t.timestamps
     end
