5ec911286f3f5e09a040f11d9ec2f56d6124a652
Eumir Gaspar <eumir@aelogica.com>
2014-05-07 00:10:36 +0800

    Added capybara and features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:new] do
+    collection do
+      get 'register' => 'users#new'
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
